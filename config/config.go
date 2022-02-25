27e6e470a944d97b7f86b4646b77f5c3a563d194
inasalifatus <inasalifatus@gmail.com>
2022-02-25 18:20:54 +0700

    init db

--- config/config.go
+++ config/config.go
@@ -5,9 +5,13 @@ import (
 	"os"
 	"strconv"
 
+	"github.com/inasalifatus/bank-payment/models"
 	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
+var DB *gorm.DB
 var PORT int
 
 func LoadEnv(key string) string {
@@ -17,7 +21,21 @@ func LoadEnv(key string) string {
 	return os.Getenv(key)
 }
 
+func InitDB() {
+	//databaseURL := os.Getenv("DATABASE_URL")
+	var err error
+	DB, err = gorm.Open(mysql.Open(LoadEnv("DATABASE_URL")), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	InitMigrate()
+}
+
 func InitPort() {
 	PORT, _ = strconv.Atoi(LoadEnv("PORT"))
 
+}
+
+func InitMigrate() {
+	DB.AutoMigrate(&models.Users{})
 }
